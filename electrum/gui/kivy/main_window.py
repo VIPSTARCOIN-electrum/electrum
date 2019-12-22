fe911cc1ae21a1be793d0f12964599e874fe1aa3
y-chan <yuto_tetuota@yahoo.co.jp>
2019-12-22 10:38:10 +0900

    fix name

--- electrum/gui/kivy/main_window.py
+++ electrum/gui/kivy/main_window.py
@@ -860,7 +860,7 @@ class ElectrumWindow(App):
                 from plyer import notification
             icon = (os.path.dirname(os.path.realpath(__file__))
                     + '/../../' + self.icon)
-            notification.notify('Electrum', message,
+            notification.notify('Electrum-VIPS', message,
                             app_icon=icon, app_name='Electrum for VIPSTARCOIN')
         except ImportError:
             Logger.Error('Notification: needs plyer; `sudo python3 -m pip install plyer`')
